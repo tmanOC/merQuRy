5dc80a8ee9023861e6a45608a795e3ade2472c04
Tielman Janse van Vuuren <vanvuuren.tielman@gmail.com>
2021-01-28 22:53:03 +0200

    Initial Commit

---
+++ merQuRy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  merQuRy
+//
+//  Created by Tielman Janse van Vuuren on 2021/01/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ merQuRyTests/merQuRyTests.swift
@@ -0,0 +1,33 @@
+//
+//  merQuRyTests.swift
+//  merQuRyTests
+//
+//  Created by Tielman Janse van Vuuren on 2021/01/28.
+//
+
+import XCTest
+@testable import merQuRy
+
+class merQuRyTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
